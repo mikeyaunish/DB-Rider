b03594a2d7a8cd468dd85f6398a17cf3e176a1e6
Mike Yaunish <mike.yaunish@gmail.com>
2017-11-09 14:32:14 -0700

    minor config file updates

--- db-overlays/billing_complete/invoice/listing-layout.r
+++ db-overlays/billing_complete/invoice/listing-layout.r
@@ -1,2 +1,2 @@
-[heading "Status" data [to-human "Status"] format [info 250 edge [size: 1x1] left]] 
-[heading "Company" data [to-human "Company"] format [info 250 edge [size: 1x1] left]]
+[heading "Status" data [to-human "Status"] format [info 200 edge [size: 1x1] left]] 
+[heading "Company" data [to-human "Company"] format [info 200 edge [size: 1x1] left]]
