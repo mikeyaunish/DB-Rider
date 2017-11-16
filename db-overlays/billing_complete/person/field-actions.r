081272978675bc3c3eec83580971a46a01086014
Mike Yaunish <mike.yaunish@gmail.com>
2017-11-16 00:25:51 -0700

    update auto generated code

--- db-overlays/billing_complete/person/field-actions.r
+++ db-overlays/billing_complete/person/field-actions.r
@@ -1,19 +1,15 @@
-; DATABASE:billing TABLE:person
-
 
 "Address" [
     assist-button [
-        ;***** ACTION:assist-button TABLE:person FIELD:Address GENERATED_CODE from relationship file. Do Not Modify this comment line. Modify the code below all you want.
-        if ((a: request-db-list/one-click/size { select Street1,City,ID from address ORDER by Street1,City ASC } "Select an address" 300x300 ) <> none) [ set-field this-field last a  ]
-        set-focus next-field
-        ;***** END_OF_GENERATED_CODE ACTION:assist-button TABLE:person FIELD:Address
+        comment {***** ACTION:assist-button TABLE:person FIELD:Address GENERATED_CODE from relationship file. Do Not Modify this comment line. Modify the code below all you want.} 
+        if ((a: request-db-list/one-click { select Street1,City,ID from address ORDER by Street1,City ASC } "Select an address") <> none) [set-field this-field last a] 
+        set-focus next-field 
     ]
-]
+] 
 "Company" [
     assist-button [
-        ;***** ACTION:assist-button TABLE:person FIELD:Company GENERATED_CODE from relationship file. Do Not Modify this comment line. Modify the code below all you want.
-        if ((a: request-db-list/one-click { select Name,ID from company ORDER by Name ASC } "Select a company" ) <> none) [ set-field this-field last a  ]
-        set-focus next-field
-        ;***** END_OF_GENERATED_CODE ACTION:assist-button TABLE:person FIELD:Company
+        comment {***** ACTION:assist-button TABLE:person FIELD:Company GENERATED_CODE from relationship file. Do Not Modify this comment line. Modify the code below all you want.} 
+        if ((a: request-db-list/one-click " select Name,ID from company ORDER by Name ASC " "Select a company") <> none) [set-field this-field last a] 
+        set-focus next-field 
     ]
 ]
